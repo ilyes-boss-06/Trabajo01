380c81674c9fb777b6a92f56047ce4f1705c56c1
JuanDi240 <juandavidpinzonerazo650@gmail.com>
2026-09-21 15:58:27 +0200

    ModeloLibro listo

--- src/trabajo1/utilidades/ModeloLibro.java
+++ src/trabajo1/utilidades/ModeloLibro.java
@@ -9,5 +9,59 @@ package trabajo1.utilidades;
  * @author 2DAM
  */
 public class ModeloLibro {
-    
+String id;
+String titulo;
+String autor;
+double precio;
+int stock;
+
+    public ModeloLibro(String id, String titulo, String autor, double precio, int stock) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+
 }
