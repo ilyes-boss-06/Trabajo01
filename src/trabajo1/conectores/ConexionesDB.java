4e1f8dc94b58655466af78a6e9e6420cde5a4594
JuanDi240 <juandavidpinzonerazo650@gmail.com>
2026-09-21 16:22:42 +0200

    ConexionesDB actualizado (Hardcodeado,necesita arreglarse), LibroRepository hecho

--- src/trabajo1/conectores/ConexionesDB.java
+++ src/trabajo1/conectores/ConexionesDB.java
@@ -4,10 +4,26 @@
  */
 package trabajo1.conectores;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author BOSS
  */
 public class ConexionesDB {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/reto_ventas_dao";
+	private static final String USER = "dam";
+	private static final String PASS = "1234";
+
+	public static Connection getConnection() {
+		Connection con = null; 
+		try {
+			con = DriverManager.getConnection(URL, USER, PASS);
+		} catch (SQLException e) {
+	System.out.println("Error al conectar con la base de datos: " + e.getMessage());
+		}
+		return con;
+	}
 }
